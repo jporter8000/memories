5b74304473df77c0bb683c55cee23c1dba7673d7
Jeff Porter <jporter8000@gmail.com>
2020-03-29 13:24:08 -0500

    next attempt

--- memories/memories/settings.py
+++ memories/memories/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = '$=rylvk4u-e4r)d8c*924y8fqjbpj2w%-=3fsw)6e&gtebmgme'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['jeffwingwarp.pythonanywhere.com']
 
 # Application definition
 
