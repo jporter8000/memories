c33ba9aace819c80953da75a2383cc955abfbc0c
Jeff Porter <jporter8000@gmail.com>
2020-06-08 13:46:45 -0500

    added classes to buttons

--- memories/templates/memories_app/ReleaseNotes.py
+++ memories/templates/memories_app/ReleaseNotes.py
@@ -2,3 +2,11 @@
 # all js viles
 # all css files
 # be careful because there is one minifying site that creates an error - it doesn't handle a / correctly
+
+
+
+# buttons need to contain classes to trigger GA Events. The classes are:
+# - download-button-1
+# - download-button-2
+# - download-button-3
+# - download-button-nav
